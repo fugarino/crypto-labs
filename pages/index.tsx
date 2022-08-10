9e7cd4fb8c38fb57b8d625a47a419363c1370951
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-08-10 07:36:54 -0400

    build: tailwind config

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import PrimaryLayout from "../components/layouts/primary/PrimaryLayout";
 import { NextPageWithLayout } from "./page";
 
 const Home: NextPageWithLayout = () => {
-  return <div>main content</div>;
+  return <div className="bg-red-500">main content</div>;
 };
 
 export default Home;
