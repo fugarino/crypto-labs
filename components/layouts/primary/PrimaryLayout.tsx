42f8e5f5e77a20a25f77db8d97b279ba1cb9185a
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-08-11 07:34:54 -0400

    feat: page layout expamle

--- components/layouts/primary/PrimaryLayout.tsx
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -1,21 +1,23 @@
-export interface IPrimaryLayout extends React.ComponentPropsWithoutRef<"div"> {
-  justify?: "items-center" | "items-start";
+import Head from "next/head";
+import Footer from "../../navigation/footer/Footer";
+import Header from "../../navigation/header/Header";
+
+export interface IPrimaryLayout {
+  children: React.ReactNode;
 }
 
-const PrimaryLayout: React.FC<IPrimaryLayout> = ({
-  children,
-  justify = "items-center",
-  ...divProps
-}) => {
+const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
   return (
     <>
-      <nav>navbar</nav>
-      <title>NextJs Fullstack App Template</title>
-      <div {...divProps} className={`min-h-screen flex flex-col ${justify}`}>
-        <main className="px-5">{children}</main>
-        <div className="m-auto" />
+      <Head>
+        <title>NextJs App</title>
+      </Head>
+      <div className="min-h-screen flex flex-col items-center">
+        <Header />
+        <main>{children}</main>
+        <div className="m-auto"></div>
+        <Footer />
       </div>
-      <footer>footer</footer>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import PrimaryLayout from "../components/layouts/primary/PrimaryLayout";
+import Search from "../components/utitity/search/Search";
 import { NextPageWithLayout } from "./page";
 
 const Home: NextPageWithLayout = () => {
-  return <div className="bg-red-500">main content</div>;
+  const { locale } = useRouter();
+
+  return (
+    <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
+      logo
+      <Search />
+      <p>Google offered in: </p>
+      <Link href="/" locale={locale === "en" ? "fr" : "en"}>
+        <a className="underline text-blue-600">Francais</a>
+      </Link>
+    </section>
+  );
 };
 
 export default Home;
